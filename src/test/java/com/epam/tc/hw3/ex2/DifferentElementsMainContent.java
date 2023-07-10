cddafa073e6a8661b353ca51eb085a066b8b0972
JulKlim <jklimmov@gmail.com>
2023-07-10 22:52:21 +0300

    Refactored 2 Exercises from Homework 2

---
+++ src/test/java/com/epam/tc/hw3/ex2/DifferentElementsMainContent.java
@@ -0,0 +1,59 @@
+package com.epam.tc.hw3.ex2;
+
+import java.time.Duration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+public class DifferentElementsMainContent {
+    final WebDriver driver;
+    final WebDriverWait wait;
+
+    public DifferentElementsMainContent(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    public void selectWaterAndWind() {
+        WebElement checkboxWater = wait.until(
+                ExpectedConditions.presenceOfElementLocated(
+                        By.xpath("//div[@class='checkbox-row']/*[1]")));
+        if (checkboxWater.getText().equals("Water")) {
+            checkboxWater.click();
+        }
+
+        WebElement checkboxWind = wait.until(
+                ExpectedConditions.presenceOfElementLocated(
+                        By.xpath("//div[@class='checkbox-row']/*[3]")));
+        if (checkboxWind.getText().equals("Wind")) {
+            checkboxWind.click();
+        }
+    }
+
+    public void selectRadioSelen() {
+        WebElement radioSelen = wait.until(
+                ExpectedConditions.elementToBeClickable(
+                        By.cssSelector("div:nth-child(3) > label:nth-child(4)")));
+        if (radioSelen.getText().equals("Selen")) {
+            radioSelen.click();
+        }
+    }
+
+    public void selectDropdownYellow() {
+        WebElement colorsDropdown = wait.until(
+                ExpectedConditions.elementToBeClickable(
+                        By.cssSelector(".colors")));
+        colorsDropdown.click();
+
+        WebElement yellowColor = wait.until(
+                ExpectedConditions.elementToBeClickable(
+                        By.xpath("//select[@class='uui-form-element']/*[4]")));
+        if (yellowColor.getText().equals("Yellow")) {
+            yellowColor.click();
+        }
+    }
+
+}
